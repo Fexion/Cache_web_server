c38dbfd5b06dd8d7a4702ec03d8ba8233bb20ec2
Alexander Smirnov <fexion1@gmail.com>
2016-11-11 04:10:21 +0300

    a

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include "easy.h"
 
-void printing(int n) {
-	for (int i = 0; i < n; ++i) {
-		std::cout << i * k << std::endl;
-	}
-}
-
 int main() {
 	std::cout << "Hello World !!\n";
 	printing(5);
---
+++ easy.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "easy.h"
+void printing(int n) {
+	for (int i = 0; i < n; ++i) {
+		std::cout << i * k << std::endl;
+	}
+}
